cacfcc533ea37921f5540c8cf0dbeef2fb22f6e7
Douglas Boardman <46564691+douglasboardman@users.noreply.github.com>
2024-04-03 18:03:07 -0300

    commit

--- processa.js
+++ processa.js
@@ -1,30 +1,47 @@
 //  callback > promise > async/await
 
 const fs = require('fs');
-
-console.log(1)
+const arqs = ['./arquivos/arq1.txt', './arquivos/arq2.txt'];
 
 // function callback(err, contents) {
-//     console.log(err, String(contents));
-// }
-
-// fs.readFile('./arquivos/pessoas.csv', (err, contents) => {
-//     fs.readFile('./arquivos/mulheres.csv', (err, contents2) => {
-//         console.log(err, String(contents))
-//         console.log(err, String(contents2))
-//     })
+    //     console.log(err, String(contents));
+    // }
+    
+    // fs.readFile('./arquivos/pessoas.csv', (err, contents) => {
+        //     fs.readFile('./arquivos/mulheres.csv', (err, contents2) => {
+            //         console.log(err, String(contents))
+            //         console.log(err, String(contents2))
+            //     })
+            // })
+            
+// const read = fileUrl => new Promise((resolve, reject) => {
+//     fs.readFile(fileUrl, (err, contents) => {
+//         if(err) reject(err);
+//         resolve(contents);
+//     });
 // })
 
-const read = fileUrl => new Promise((resolve, reject) => {
+const read = fileUrl => {
     fs.readFile(fileUrl, (err, contents) => {
-        if(err) reject(err);
-        resolve(contents);
+        if(err) console.log(err);
+        return contents;
     });
-})
+}
 
-read('./arquivos/mulheres.csv')
-    .then(result => console.log(String(result)))
+// read('./arquivos/mulheres.csv')
+//     .then(result => console.log(String(result)))
+console.log(1)
 
 console.log(2);
 
 console.log(3);
+
+console.log(read(arqs[0]));
+
+console.log(7);
+
+console.log(8);
+
+console.log(9);
+
+console.log(read(arqs[1]));
